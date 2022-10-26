7e5e088f4d5414dc3668c61e2ddbdbb451c94362
FPRsharing <115086704+FPTproject@users.noreply.github.com>
2022-10-26 19:07:23 +0700

    update contact and footer

--- resources/views/User/main/footer.blade.php
+++ resources/views/User/main/footer.blade.php
@@ -1,5 +1,224 @@
 <!-- JavaScript Bundle with Popper -->
-
+<style>
+    /*---------------------
+      Footer
+    -----------------------*/
+    
+    .footer {
+        background: #fff;
+        padding-top: 30px;
+        padding-bottom: 0;
+        font-family: "Cairo", sans-serif;
+        -webkit-font-smoothing: antialiased;
+    }
+    
+    .footer__about {
+        margin-bottom: 30px;
+    }
+    
+    .footer__about ul li {
+        font-size: 14px;
+        color: #1c1c1c;
+        line-height: 36px;
+        list-style: none;
+        font-family: "Cairo", sans-serif;
+        -webkit-font-smoothing: antialiased;
+    }
+    
+    .footer__about__logo {
+        margin-bottom: 15px;
+    }
+    
+    .footer__about__logo a {
+        display: inline-block;
+    }
+    
+    .footer__widget {
+        margin-bottom: 30px;
+        overflow: hidden;
+    }
+    
+    .footer__widget h6 {
+        color: #1c1c1c;
+        font-weight: 700;
+        margin-bottom: 10px;
+    }
+    
+    .footer__widget ul {
+        width: 50%;
+        float: left;
+    }
+    
+    .footer__widget ul li {
+        list-style: none;
+        font-family: "Cairo", sans-serif;
+        -webkit-font-smoothing: antialiased;
+    }
+    
+    .footer__widget ul li a {
+        color: #1c1c1c;
+        font-size: 14px;
+        line-height: 32px;
+    }
+    
+    .footer__widget p {
+        font-size: 14px;
+        color: #1c1c1c;
+        margin-bottom: 30px;
+    }
+    
+    .footer__widget form {
+        position: relative;
+        margin-bottom: 30px;
+    }
+    
+    .footer__widget form input {
+        width: 100%;
+        font-size: 16px;
+        padding-left: 20px;
+        color: #1c1c1c;
+        height: 46px;
+        border: 1px solid #ededed;
+    }
+    
+    .footer__widget form input::placeholder {
+        color: #1c1c1c;
+    }
+    
+    .footer__widget form button {
+        position: absolute;
+        right: 0;
+        top: 0;
+        padding: 0 26px;
+        height: 100%;
+    }
+    
+    .footer__widget .footer__widget__social a {
+        display: inline-block;
+        height: 41px;
+        width: 41px;
+        font-size: 16px;
+        color: #404040;
+        border: 1px solid #ededed;
+        border-radius: 50%;
+        line-height: 38px;
+        text-align: center;
+        background: #ffffff;
+        -webkit-transition: all, 0.3s;
+        -moz-transition: all, 0.3s;
+        -ms-transition: all, 0.3s;
+        -o-transition: all, 0.3s;
+        transition: all, 0.3s;
+        margin-right: 10px;
+    
+    }
+    
+    d .footer__widget .footer__widget__social a:last-child {
+        margin-right: 0;
+    }
+    
+    .footer__widget .footer__widget__social a:hover {
+        background: #17a2b8;
+        color: #ffffff;
+        border-color: #ffffff;
+    }
+    
+    .footer__copyright {
+        border-top: 1px solid #ebebeb;
+        padding: 15px 0;
+        overflow: hidden;
+        margin-top: 20px;
+    }
+    
+    .footer__copyright__text {
+        font-size: 14px;
+        color: #1c1c1c;
+        float: left;
+        line-height: 25px;
+    }
+    
+    .footer__copyright__payment {
+        float: right;
+    }
+    
+    .links {
+        padding-top: 30px;
+    }
+    </style>
+    <footer class="footer spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="footer__about">
+                        <div class="footer__about__logo">
+                            <a href=""><img src="images/mim.png" width="200px" alt=""></a>
+                        </div>
+                        <ul>
+                            <li><b>Address:</b> 590 Cách mạng tháng 8 - district 3- HCM city</li>
+    <li><b>Phone:</b> +65 11.188.888</li>
+                            <li><b>Email:</b> hello@colorlib.com</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 offset-lg-1 links">
+                    <div class="footer__widget">
+    
+                        <ul>
+                            <h6>Links</h6>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="{{ url('/about') }}">About Us</a></li>
+                            <li><a href="{{ url('/contact') }}">Contact us</a></li>
+                            <li><a href="#">Furniture menu</a></li>
+    
+    
+                        </ul>
+                        <ul>
+                            <h6>Policy</h6>
+                            <li><a href="#">Payment policy</a></li>
+                            <li><a href="#">Shipping Policy</a></li>
+                            <li><a href="#">Warranty Policy</a></li>
+    
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12 links ">
+                    <div class="footer__widget">
+    
+                        <div class="footer__widget__social">
+                            <ul>
+                                <li> <a href="#"><i class="fa fa-facebook"></i> </a>Facebook</li>
+                                <li><a href="#"><i class="fa fa-instagram"></i></a>Instagram</li>
+                                <li> <a href="#"><i class="fa fa-twitter"></i> </a>Twitter</li>
+                                <li> <a href="#"><i class="fa fa-linkedin"></i> </a>Linkedin</li>
+                                <li> <a href="#"><i class="fa fa-medium"></i> </a>Medium</li>
+    
+                            </ul>
+    
+    
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer__copyright">
+                        <div class="footer__copyright__text">
+                            <p>
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                Copyright &copy;<script>
+                                document.write(new Date().getFullYear());
+                                </script> All rights reserved | This template is made with <i class="fa fa-heart"
+                                    aria-hidden="true"></i> by <a href="" target="_blank">DTBM</a>
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            </p>
+                        </div>
+                        <div class="footer__copyright__payment"><img src="images/payment-item.png" alt=""></div>
+                    </div>
+                </div>
+            </div>
+    
+        </div>
+    </footer>
 <!-- JavaScript Bundle with Popper -->
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
--- resources/views/User/pages/contact/contact.blade.php
+++ resources/views/User/pages/contact/contact.blade.php
@@ -2,8 +2,182 @@
 @section('content')
 
 <!-- feature -->
-<div class="text-center pt-5">
-    <h1>Contact Page</h1>
- </div>
+<style>
+    .contact {
+        padding-top: 80px;
+        padding-bottom: 50px;
+    }
+
+    .contact__widget {
+        margin-bottom: 30px;
+    }
+
+    .contact__widget span {
+        font-size: 36px;
+        color: #17a2b8;
+        font-family: 'Heebo', sans-serif;
+        font-family: 'Source Serif Pro', serif;
+    }
+
+    .contact__widget h4 {
+        color: #1c1c1c;
+        font-weight: 700;
+        margin-bottom: 6px;
+        margin-top: 18px;
+    }
+
+    .contact__widget p {
+        color: #666666;
+        margin-bottom: 0;
+    }
+
+    .map {
+        height: 500px;
+        position: relative;
+    }
+
+    .map iframe {
+        width: 100%;
+    }
+
+    .map .map-inside {
+        position: absolute;
+        left: 50%;
+        top: 160px;
+        -webkit-transform: translateX(-175px);
+        -ms-transform: translateX(-175px);
+        transform: translateX(-175px);
+    }
+
+    .map .map-inside i {
+        font-size: 48px;
+        color: #7fad39;
+        position: absolute;
+        bottom: -75px;
+        left: 50%;
+        -webkit-transform: translateX(-18px);
+        -ms-transform: translateX(-18px);
+        transform: translateX(-18px);
+    }
+
+    .map .map-inside .inside-widget {
+        width: 350px;
+        background: #ffffff;
+        text-align: center;
+        padding: 23px 0;
+        position: relative;
+        z-index: 1;
+        -webkit-box-shadow: 0 0 20px 5px rgba(12, 7, 26, 0.15);
+        box-shadow: 0 0 20px 5px rgba(12, 7, 26, 0.15);
+    }
+
+    .map .map-inside .inside-widget:after {
+        position: absolute;
+        left: 50%;
+        bottom: -30px;
+        -webkit-transform: translateX(-6px);
+        -ms-transform: translateX(-6px);
+        transform: translateX(-6px);
+        border: 12px solid transparent;
+        border-top: 30px solid #ffffff;
+        content: "";
+        z-index: -1;
+    }
+
+    .map .map-inside .inside-widget h4 {
+        font-size: 22px;
+        font-weight: 700;
+        color: #1c1c1c;
+        margin-bottom: 4px;
+    }
+
+    .map .map-inside .inside-widget ul li {
+        list-style: none;
+        font-size: 16px;
+        color: #666666;
+        line-height: 26px;
+    }
+
+    .text {
+        color: rgb(161, 166, 171);
+        position: absolute;
+        top: 200px;
+        right: 35%;
+        font-size: 80px;
+        font-family: 'Heebo', sans-serif;
+        font-family: 'Source Serif Pro', serif;
+
+    }
+
+    .text-sologan {
+        color: #A2BDBD;
+        position: absolute;
+        top: 320px;
+        right: 25%;
+        font-size: 20px;
+        font-family: 'Heebo', sans-serif;
+        font-family: 'Source Serif Pro', serif;
+
+    }
+</style>
+<section>
+    <h1 class="text">CONTACT</h1>
+
+    <img src="images/contactmen.webp" alt="" width="100%" height="500px">
+</section>
+<section class=" contact spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                <div class="contact__widget">
+                    <span class="icon_phone">
+                        <i class="fa fa-phone"></i>
+                    </span>
+                    <h4>Phone</h4>
+                    <p>+01-3-8888-6868</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                <div class="contact__widget">
+                    <span class="icon_pin_alt">
+                        <i class="fa fa-map-marker"></i>
+                    </span>
+                    <h4>Address</h4>
+                    <p>590 Cach Mang thang 8 - District 3 -HCM city</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                <div class="contact__widget">
+                    <span class="icon_clock_alt"><i class="fa fa-clock-o"></i></span>
+                    <h4>Open time</h4>
+                    <p>10:00 am to 23:00 pm</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 text-center">
+                <div class="contact__widget">
+                    <span class="icon_mail_alt"><i class="fa fa-envelope" aria-hidden="true"></i></span>
+                    <h4>Email</h4>
+                    <p>hello@colorlib.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="map">
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.246300131862!2d106.65209221471854!3d10.792438492310668!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752935383feb7f%3A0xfebc21cd3e7bfe9c!2zNTkwIMSQLiBDw6FjaCBN4bqhbmcgVGjDoW5nIDgsIFBoxrDhu51uZyA3LCBRdeG6rW4gMywgVGjDoG5oIHBo4buRIEjhu5MgQ2jDrSBNaW5oLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1666785609235!5m2!1svi!2s"
+        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <div class="map-inside">
+        <i class="icon_pin"></i>
+        <div class="inside-widget">
+            <h4>TP.HCM</h4>
+            <ul>
+                <li>Phone: +12-345-6789</li>
+                <li>Add: 16 Creek Ave. Farmingdale, NY</li>
+            </ul>
+        </div>
+    </div>
+</div>
 
 @endsection
